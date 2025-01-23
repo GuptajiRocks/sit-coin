5621bcf148a81c21367e6dc430af59cfa97fa215
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-01-23 10:18:29 +0530

    three pages done

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    return render_template("index.html")
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
