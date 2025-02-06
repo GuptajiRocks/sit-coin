c7e380f6a7198698c61310d5a1996eab5edd19e4
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-02-06 15:18:05 +0530

    random shit

--- misc.py
+++ misc.py
@@ -1,30 +1,39 @@
-import os
-from psycopg2 import pool, sql
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
-
-# Get database URL from .env
-connection_string = os.getenv('DATABASE_URL')
-
-# Initialize connection pool
-connection_pool = pool.SimpleConnectionPool(1, 10, connection_string)
-
-# Check if the pool was created successfully
-# if connection_pool:
-#     print("Connection pool created successfully")
-
-# # Get a connection from the pool
-# conn = connection_pool.getconn()
-# cur = conn.cursor()
+import qrcode
+import base64
+qrdata = "https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"
+qr = qrcode.make(qrdata)
 
 
-# # Commit changes
-# conn.commit()
 
-# print("Tables created successfully!")
 
-# # Release connection back to the pool
-# cur.close()
-# connection_pool.putconn(conn)
+
+# import os
+# from psycopg2 import pool, sql
+# from dotenv import load_dotenv
+
+# # Load environment variables
+# load_dotenv()
+
+# # Get database URL from .env
+# connection_string = os.getenv('DATABASE_URL')
+
+# # Initialize connection pool
+# connection_pool = pool.SimpleConnectionPool(1, 10, connection_string)
+
+# # Check if the pool was created successfully
+# # if connection_pool:
+# #     print("Connection pool created successfully")
+
+# # # Get a connection from the pool
+# # conn = connection_pool.getconn()
+# # cur = conn.cursor()
+
+
+# # # Commit changes
+# # conn.commit()
+
+# # print("Tables created successfully!")
+
+# # # Release connection back to the pool
+# # cur.close()
+# # connection_pool.putconn(conn)
