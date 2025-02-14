ffb2bac11223b0f21d2cb8a5087f5c04ac86e887
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-02-14 12:16:50 +0530

    models.py commented

--- models.py
+++ models.py
@@ -1,18 +1,18 @@
-import sqlite3
+# import sqlite3
 
-# Create database and tables
-connection = sqlite3.connect('database.db')
-cursor = connection.cursor()
+# # Create database and tables
+# connection = sqlite3.connect('database.db')
+# cursor = connection.cursor()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    phone INTEGER UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    balance REAL NOT NULL
-)
-''')
+# cursor.execute('''
+# CREATE TABLE IF NOT EXISTS users (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     username TEXT NOT NULL,
+#     phone INTEGER UNIQUE NOT NULL,
+#     password TEXT NOT NULL,
+#     balance REAL NOT NULL
+# )
+# ''')
 
-connection.commit()
-connection.close()
+# connection.commit()
+# connection.close()
